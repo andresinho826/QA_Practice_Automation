d484054838dc8ef87984588db318c4914898feb2
Castillo <andresinho826@gmail.com>
2025-04-01 19:52:34 -0500

    saudemo_test class

--- src/test/java/Saudemo/Saudemo_Lab.java
+++ src/test/java/Saudemo/Saudemo_Lab.java
@@ -18,7 +18,7 @@ public class Saudemo_Lab {
 
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -29,30 +29,62 @@ public class Saudemo_Lab {
 
     @Test
     public void loginTest() throws InterruptedException {
-        /*
-        #login-button
-        #password
-        #user-name
-
-        standard_user
-        secret_sauce
-         */
 
         WebElement usuario = driver.findElement(By.cssSelector("#user-name"));
         WebElement pass = driver.findElement(By.cssSelector("#password"));
         WebElement btnLogin = driver.findElement(By.cssSelector("#login-button"));
-
         usuario.sendKeys("standard_user");
-
-        //Thread.sleep(100);
         pass.sendKeys("secret_sauce");
-        //Thread.sleep(100);
         btnLogin.click();
 
     }
 
+    @Test
+    public void buyAProduct(){
+        WebElement productPage = driver.findElement(By.xpath("//*[contains(text(), 'Products')]"));
+        WebElement addToCartBtn = driver.findElement(By.cssSelector("#add-to-cart-sauce-labs-backpack"));
+        WebElement shoppingCartBtn = driver.findElement(By.cssSelector(".shopping_cart_link"));
+        WebElement yourCartPage = driver.findElement(By.xpath("//span[@class='title' and text()='Your Cart']"));
+        WebElement checkoutBtn = driver.findElement(By.cssSelector("#checkout"));
+        WebElement checkOutInfoPage = driver.findElement(By.xpath("//span[@class='title' and text()='Checkout: Your Information']"));
+        WebElement firstName = driver.findElement(By.cssSelector("#first-name"));
+        WebElement lastName = driver.findElement(By.cssSelector("#last-name"));
+        WebElement zipCode = driver.findElement(By.cssSelector("#postal-code"));
+        WebElement continueBtn = driver.findElement(By.cssSelector("#continue"));
+        WebElement overviewPage = driver.findElement(By.xpath("//span[@class='title' and text()='Checkout: Overview']"));
+        WebElement finishBtn = driver.findElement(By.cssSelector("#finish"));
+        WebElement completePage = driver.findElement(By.xpath("//span[@class='title' and text()='Checkout: Complete!']"));
+        WebElement successOrder = driver.findElement(By.cssSelector(".complete-header"));
+        WebElement backHomeBtn = driver.findElement(By.cssSelector("#back-to-products"));
+        WebElement homePage = driver.findElement(By.cssSelector(".app_logo"));
+
+
+        productPage.isDisplayed();
+        addToCartBtn.click();
+        shoppingCartBtn.click();
+
+        yourCartPage.isDisplayed();
+        checkoutBtn.click();
+
+        checkOutInfoPage.isDisplayed();
+        firstName.sendKeys("cualquier");
+        lastName.sendKeys("cosa");
+        zipCode.sendKeys("100221525");
+        continueBtn.click();
+
+        overviewPage.isDisplayed();
+        finishBtn.click();
+
+        completePage.isDisplayed();
+        successOrder.isDisplayed();
+        backHomeBtn.click();
+
+        homePage.isDisplayed();
+    }
+
+
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();  // cierra todas las ventanas
         //driver.close(); // solo cierrra la ventana actual
     }
---
+++ src/test/java/Saudemo/Saudemo_test.java
@@ -0,0 +1,90 @@
+package Saudemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Saudemo_test {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    String urlPage = "https://www.saucedemo.com/";
+    String chromeDriverPath = "src/test/resources/chromedriver.exe";
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Esperas explícitas
+        driver.manage().window().maximize();
+        driver.get(urlPage);
+    }
+
+    @Test
+    public void loginTest() {
+        WebElement usuario = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name")));
+        WebElement pass = driver.findElement(By.id("password"));
+        WebElement btnLogin = driver.findElement(By.id("login-button"));
+
+        usuario.sendKeys("standard_user");
+        pass.sendKeys("secret_sauce");
+        btnLogin.click();
+
+        // Validación de inicio de sesión
+        WebElement productPage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(), 'Products')]")));
+        Assert.assertTrue(productPage.isDisplayed(), "No se encontró la página de productos, el login falló.");
+    }
+
+    @Test
+    public void buyAProduct() {
+        loginTest(); // Reutilizar el login antes de comprar un producto
+
+        WebElement addToCartBtn = wait.until(ExpectedConditions.elementToBeClickable(By.id("add-to-cart-sauce-labs-backpack")));
+        addToCartBtn.click();
+
+        WebElement shoppingCartBtn = driver.findElement(By.cssSelector(".shopping_cart_link"));
+        shoppingCartBtn.click();
+
+        WebElement checkoutBtn = wait.until(ExpectedConditions.elementToBeClickable(By.id("checkout")));
+        checkoutBtn.click();
+
+        WebElement firstName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("first-name")));
+        WebElement lastName = driver.findElement(By.id("last-name"));
+        WebElement zipCode = driver.findElement(By.id("postal-code"));
+        WebElement continueBtn = driver.findElement(By.id("continue"));
+
+        firstName.sendKeys("cualquier");
+        lastName.sendKeys("cosa");
+        zipCode.sendKeys("100221525");
+        continueBtn.click();
+
+        WebElement finishBtn = wait.until(ExpectedConditions.elementToBeClickable(By.id("finish")));
+        finishBtn.click();
+
+        WebElement successOrder = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".complete-header")));
+        Assert.assertTrue(successOrder.isDisplayed(), "No se encontró el mensaje de compra exitosa.");
+
+        WebElement backHomeBtn = driver.findElement(By.id("back-to-products"));
+        backHomeBtn.click();
+
+        WebElement homePage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".app_logo")));
+        Assert.assertTrue(homePage.isDisplayed(), "No se regresó a la página principal.");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
